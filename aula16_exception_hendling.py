014be18be9e0083a9dfbb88d30fa38daa27b6554
jwellingtonjuniordev-dev <jwellingtonjuniordev@gmail.com>
2026-06-14 19:02:38 +0100

    added two classes User and ShoppingCart

--- aula16_exception_hendling.py
+++ aula16_exception_hendling.py
@@ -62,3 +62,67 @@ calculator1.divide(5, 0)
 # try
 # except ValueError
 
+class User:
+    def __init__(self):
+        self.age = 0
+
+    def set_age(self):
+        entry = input(f"Insert your age ")
+        try:
+            self.age = int(entry)
+            print(f"Age registered successfully")
+        except ValueError:
+            print(f"Invalid age")
+
+user1 = User()
+user1.set_age()
+
+# Mini Challenge
+# Crie: class ShoppingCart:
+# Atributos: products, prices
+# Método: add_product()
+# Deve pedir: Product name:, Product price:
+# Se o preço não for numérico: Invalid price.
+# Caso contrário, adicionar o produto.
+# Crie também:
+# show_products()
+# Mostrando: Laptop - $1200, Mouse - $20, Keyboard - $80
+
+class ShoppingCart:
+    def __init__(self):
+        self.list_products = []
+        self.produtct_name = ''
+        self.product_price = 0
+        self.products = {}
+
+    def add_product(self):
+        prod_name = input(f"Insert the product name:\n")
+        prod_price = input(f"Insert the price:\n")
+
+        if prod_name.isdigit():
+            print(f"The product's name cannot have numbers, please insert correct name")
+        else:
+            self.product_name = prod_name.capitalize()
+
+        try:
+            self.product_price = float(prod_price)
+            self.products = {"name": self.product_name, "price":self.product_price}
+            self.list_products.append(self.products.copy())
+            print(f"Product added")
+        except ValueError:
+            print(f"Invalid price")
+
+    def show_products(self):
+
+        for product in self.list_products:
+            print(
+                f"Product: {product["name"]} | "
+                f"Price ${product["price"]:.2f}"
+            )
+
+shopping_cart = ShoppingCart()
+shopping_cart.add_product()
+shopping_cart.add_product()
+shopping_cart.add_product()
+shopping_cart.show_products()
+
